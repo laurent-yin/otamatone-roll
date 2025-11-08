ae65ea1e85949f6fa2d27675a72ea1ce60dae4ab
Laurent Yin <williethewimp@gmail.com>
2025-11-08 17:55:56 +0100

    use abcjs and render a dummy abc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import abcjs from 'abcjs';
 
 function App() {
-  const value = 'World';
-  return <div>Ohayou Gozaimasu {value}</div>;
+  const abc = `X:1
+T:Notes / pitches
+M:C
+L:1/4
+K:C treble
+C, D, E, F, | G, A, B, C | D E F G | A B c d | e f g a | b c' d' e' | f' g' a' b' |]`;
+
+  useEffect(() => {
+    abcjs.renderAbc('abcjs-container', abc);
+  }, []);
+
+  return (
+    <div>
+      <h1>Otamatone Roll</h1>
+      <div id="abcjs-container"></div>
+    </div>
+  );
 }
 
 export default App;
