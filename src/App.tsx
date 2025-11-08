92b73d183158252dcfa3db125cee2e50507a6a8d
Laurent Yin <williethewimp@gmail.com>
2025-11-08 18:06:58 +0100

    move to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import abcjs from 'abcjs';
 
-function App() {
-  const abc = `X:1
+const abc: string = `X:1
 T:Notes / pitches
 M:C
 L:1/4
 K:C treble
 C, D, E, F, | G, A, B, C | D E F G | A B c d | e f g a | b c' d' e' | f' g' a' b' |]`;
 
+const App: React.FC = () => {
   useEffect(() => {
     abcjs.renderAbc('abcjs-container', abc);
   }, []);
@@ -19,6 +19,6 @@ C, D, E, F, | G, A, B, C | D E F G | A B c d | e f g a | b c' d' e' | f' g' a' b
       <div id="abcjs-container"></div>
     </div>
   );
-}
+};
 
 export default App;
