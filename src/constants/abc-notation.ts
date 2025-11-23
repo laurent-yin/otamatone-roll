e7ee5b1efed2fc15f9a29a2c5b3e7b64cd3ee3bd
Laurent Yin <williethewimp@gmail.com>
2025-11-23 23:05:52 +0100

    better sound font for the sample track

--- src/constants/abc-notation.ts
+++ src/constants/abc-notation.ts
@@ -5,7 +5,7 @@ L:1/16
 Q:1/4=80
 R:Romantic song
 K:C
-%%MIDI program 85
+%%MIDI program 100
 Z | B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
 B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
 B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
