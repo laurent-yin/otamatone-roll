6a86933a3bf000365040ad8fefaad91466d7f918
Laurent Yin <williethewimp@gmail.com>
2025-11-27 15:00:49 +0100

    split into different chunks the js files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          abcjs: ['abcjs'],
+          dockview: ['dockview-core'],
+        },
+      },
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
