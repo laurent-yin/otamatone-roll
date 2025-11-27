145a59b38c848452cacaef1dc31108a7bc29b166
Laurent Yin <williethewimp@gmail.com>
2025-11-27 12:04:26 +0100

    change default song

--- src/constants/abc-notation.ts
+++ src/constants/abc-notation.ts
@@ -1,13 +1,29 @@
-export const DEFAULT_ABC_NOTATION = `X:1
-T:Test
-M:4/4
+export const DEFAULT_ABC_NOTATION = `%%MIDI gchord fcccfccc
+
+X:1
+%%topmargin 0
+%%botmargin 0
+T:Sempre Teu
+C: Jacob do Bandolim
+P:ABBACA
+M:2/4
 L:1/16
-Q:1/4=80
-R:Romantic song
-K:C
-%%MIDI program 100
-Z | B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
-B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
-B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
-B2c_B- B2=B2 AAA2 ^G4 | ^G2A=G- G2^G2 ^FFF2 =F4 |
+Q:1/4=65
+R:Choro
+K:Dm clef=treble
+P:A
+z/A/^G/A/=B/^c/d/e/ || "Dm"f4- "Dm/C"fedf- | "B°"f4- "Gm/Bb"fed^c- | "A7"(3c2E2F2 "A/G"(3G2=c2B2 | "Dm/F"A4- (6:4AA=B^cde | 
+"Dm"f4- "Bm7b5"fedc- | "Am"cd^de- e4 | "E7/G#"z=Bcd- d4 | "A/G"zA(3=Bc^c- (3c2_B2A2 | 
+"Dm"[DAf]4- "Dm/C"[DAf]edf- | "B°"f4- "Gm/Bb"fed^c- | "A7"(3c2E2F2 "A/G"(3G2=c2B2 | "D7/F#"A3[C^FA]- "D7"[C^FA]BA [Dd]- |
+"Gm/Bb"[Dd]4 "E7"^Gc=Be- | "Am"e3[Dd]- "Gm6"[Dd]BGF- | "Dm/A"FG^GA- "A7"AEFD- | "Dm"D4 ||
+P:B
+(3A2c2B2 ||  "A7/C#"(3^C2D2^D2 "A/G"(3E2=c2B2 | "Dm/F"A4 (3d2f2_e2 | "D7/F#"(3^F2G2^G2 "D7"(3A2=f2_e2 | "Gm"d3d- d/d/=e/d/^c/d/g- |
+"Gm6/Bb"(3g2f2e2 "Em7b5"dcB[FB]- | "Dm/F"[FB]2AG FED^G,- | "E7/G#"(3^G,2[^G,E]2[A,^F]2 "E7"[=B,^G]A=B[=G^c]- | "A7"[G^c]3A =c=B_BA |
+"A7/C#"(3^C2D2^D2 "A/G"(3E2=c2B2 | "Dm/F"A4 (3d2f2_e2 | "D7/F#"(3^F2G2^G2 "D7"(3A2=f2_e2 | "Gm"d4- d/d/=e/d/^c/d/g- |
+"Gm6/Bb"g2fe "Em7b5"(3d2c2B2 | "Dm/F"B2AG FEDE- | "Em7b5"(3E2F2G2 "A7"(3A2E2F2 | "Dm"D4 ||
+P:C
+(3^F2=B2A2 [K:D] || "D/F#"[^FAe]3d "Bm7"FBA[B,DA]- | "E7"[B,DA]3^G- GcB[=Gcf]- | "A7"[Gcf]3e GdcB- | "F°"B3A "D/F#"FBAe- |
+"D"e3d "Bm6"FBAc- | "F#m/C#"c3c- c^Bcd- | "C#7/E#"d/c/^B/c/d/c/B/c/ "C#7"d/c/B/c/d/c/B/c/ | "F#m"F3^E "A7"FBA[FAe]- |
+"D"[FAe]3d "Bm7"FBA[B,DA]- | "E7"[B,DA]3^G- GcBf- | "A7"f3e =GdcB- | "G7"B3^A- "F#7"Adcg- |
+"F#/E"g3f "F#7"c^Bce- | "Bm"edcB- "G"BAGF- | "D/A"FBAC- "A7"CFED- | "D"D4 ||
 `;
